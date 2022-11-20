2f1b5732c28ebd7377263a81cd59d95ae0de0358
Nuggo <NuggoDEV@gmail.com>
2022-11-20 20:22:22 +1030

    Add more values

--- include/ModConfig.hpp
+++ include/ModConfig.hpp
@@ -6,10 +6,14 @@ DECLARE_CONFIG(ModConfig,
     CONFIG_VALUE(Enabled, bool, "Enabled", false);
     CONFIG_VALUE(LeftSaberToggle, bool, "Left Saber Enabled", true);
     CONFIG_VALUE(RightSaberToggle, bool, "Right Saber Enabled", true);
+    CONFIG_VALUE(DisableCrouch, bool, "Disable Crouch Walls", false);
+    CONFIG_VALUE(DisableWalls, bool, "Disable Walls", false);
 
     CONFIG_INIT_FUNCTION(
         CONFIG_INIT_VALUE(Enabled);
         CONFIG_INIT_VALUE(LeftSaberToggle);
         CONFIG_INIT_VALUE(RightSaberToggle);
+        CONFIG_INIT_VALUE(DisableCrouch);
+        CONFIG_INIT_VALUE(DisableWalls);
     )
 )
