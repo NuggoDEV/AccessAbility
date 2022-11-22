fa48610a9fd8a2843eb688b035d4cd8e325625a6
Nuggo <NuggoDEV@gmail.com>
2022-11-22 22:54:32 +1030

    thang (forgor what I did)

--- include/ModConfig.hpp
+++ include/ModConfig.hpp
@@ -4,9 +4,9 @@
 
 DECLARE_CONFIG(ModConfig,
     CONFIG_VALUE(Enabled, bool, "Enabled", false);
-    CONFIG_VALUE(LeftSaberToggle, bool, "Left Saber Enabled", true);
-    CONFIG_VALUE(RightSaberToggle, bool, "Right Saber Enabled", true);
+    CONFIG_VALUE(LeftSaberToggle, bool, "Left Saber Enabled", false);
+    CONFIG_VALUE(RightSaberToggle, bool, "Right Saber Enabled", false);
     CONFIG_VALUE(DisableCrouch, bool, "Disable Crouch Walls", false);
-    CONFIG_VALUE(DisableWalls, bool, "Disable Walls", false);
+    CONFIG_VALUE(NiceWalls, bool, "Makes Walls Nice", false);
     CONFIG_VALUE(YeetSliders, bool, "Removes Sliders", false);
 )
