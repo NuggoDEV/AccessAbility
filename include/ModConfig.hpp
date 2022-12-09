9e394eadf5b629bcfcf02a65d7e84bcca544aff1
NuggoDEV <NuggoDEV@gmail.com>
2022-12-10 01:41:35 +1030

    Yeet bomb stuff

--- include/ModConfig.hpp
+++ include/ModConfig.hpp
@@ -6,7 +6,6 @@ DECLARE_CONFIG(ModConfig,
     CONFIG_VALUE(Enabled, bool, "Enabled", false);
     CONFIG_VALUE(LeftSaberToggle, bool, "Left Saber Enabled", false);
     CONFIG_VALUE(RightSaberToggle, bool, "Right Saber Enabled", false);
-    CONFIG_VALUE(DisableCrouch, bool, "Disable Crouch Walls", false);
-    CONFIG_VALUE(NiceWalls, bool, "Makes Walls Nice", false);
-    CONFIG_VALUE(YeetSliders, bool, "Removes Sliders", false);
+    CONFIG_VALUE(YeetCrouchWalls, bool, "Disable Crouch Walls", false);
+    CONFIG_VALUE(YeetBombs, bool, "Yeets Bombs", false);
 )
