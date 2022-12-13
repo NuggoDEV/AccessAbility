732bb70b95cf867475e7aa141f9cff11a29d09e2
NuggoDEV <NuggoDEV@gmail.com>
2022-12-13 18:15:45 +1030

    Can't figure these arcs out jgoudfjgklfshkjlhkljgt

--- src/Hooks/SaberToggleHooks.cpp
+++ src/Hooks/SaberToggleHooks.cpp
@@ -2,17 +2,16 @@
 #include "ModConfig.hpp"
 #include "Hooks.hpp"
 
-
-#include "GlobalNamespace/GameNoteController.hpp"
 #include "GlobalNamespace/NoteData.hpp"
+#include "GlobalNamespace/NoteController.hpp"
 #include "GlobalNamespace/NoteVisualModifierType.hpp"
 #include "GlobalNamespace/ColorType.hpp"
+
+#include "GlobalNamespace/SliderController.hpp"
+#include "GlobalNamespace/SliderData.hpp"
 using namespace GlobalNamespace;
 
 using namespace UnityEngine;
-
-//NoteController noteController;
-//ColorType noteColour = noteController.noteData->colorType;
 
 MAKE_AUTO_HOOK_MATCH(NoteController_Init, &NoteController::Init, void, NoteController *self, NoteData *noteData, float worldRotation, Vector3 moveStartPos, Vector3 moveEndPos, Vector3 jumpEndPos, float moveDuration, float jumpDuration, float jumpGravity, float endRotation, float uniformScale, bool rotateTowardsPlayer, bool useRandomRotation)
 {
@@ -21,9 +20,27 @@ MAKE_AUTO_HOOK_MATCH(NoteController_Init, &NoteController::Init, void, NoteContr
     if (self->noteData->colorType == ColorType::ColorA && getModConfig().LeftSaberToggle.GetValue() && getModConfig().Enabled.GetValue())
     {
         self->get_gameObject()->SetActive(false);
+        //Object::Destroy(self);
     }
     else if (self->noteData->colorType == ColorType::ColorB && getModConfig().RightSaberToggle.GetValue() && getModConfig().Enabled.GetValue())
     {
         self->get_gameObject()->SetActive(false);
+        //Object::Destroy(self);
+    }
+}
+
+
+// Don't work sadge
+MAKE_AUTO_HOOK_MATCH(SliderController_Init, &SliderController::Init, void, SliderController *self, SliderController::LengthType lengthType, SliderData *sliderData, float worldRotation, Vector3 headNoteJumpStartPos, Vector3 tailNoteJumpStartPos, Vector3 headNoteJumpEndPos, Vector3 tailNoteJumpEndPos, float jumpDuration, float startNoteJumpGravity, float endNoteJumpGravity, float noteUniformScale)
+{
+    SliderController_Init(self, lengthType, sliderData, worldRotation, headNoteJumpStartPos, tailNoteJumpStartPos, headNoteJumpEndPos, tailNoteJumpEndPos, jumpDuration, startNoteJumpGravity, endNoteJumpGravity, noteUniformScale);
+
+    if (self->sliderData->colorType == ColorType::ColorA && getModConfig().LeftSaberToggle.GetValue() && getModConfig().Enabled.GetValue())
+    
+       self->get_gameObject()->SetActive(false);
+    
+    else if (self->sliderData->colorType == ColorType::ColorB && getModConfig().RightSaberToggle.GetValue() && getModConfig().Enabled.GetValue())
+    {
+       self->get_gameObject()->SetActive(false);
     }
 }
