4866b1ac95a3a816f6acc67387148327a76c1106
NuggoDEV <NuggoDEV@gmail.com>
2022-12-16 19:57:48 +1030

    Renamed hook name

--- src/Hooks/YeetCrouchWalls.cpp
+++ src/Hooks/YeetCrouchWalls.cpp
@@ -13,9 +13,9 @@ using namespace GlobalNamespace;
 
 using namespace UnityEngine;
 
-MAKE_AUTO_HOOK_MATCH(ObstacleController_Init, &ObstacleController::Init, void, ObstacleController *self, ObstacleData *obstacleData, float worldRotation, Vector3 startPos, Vector3 midPos, Vector3 endPos, float move1Duration, float move2Duration, float singleLineWidth, float height)
+MAKE_AUTO_HOOK_MATCH(Crouch_ObstacleController_Init, &ObstacleController::Init, void, ObstacleController *self, ObstacleData *obstacleData, float worldRotation, Vector3 startPos, Vector3 midPos, Vector3 endPos, float move1Duration, float move2Duration, float singleLineWidth, float height)
 {
-    ObstacleController_Init(self, obstacleData, worldRotation, startPos, midPos, endPos, move1Duration, move2Duration, singleLineWidth, height);
+    Crouch_ObstacleController_Init(self, obstacleData, worldRotation, startPos, midPos, endPos, move1Duration, move2Duration, singleLineWidth, height);
 
     if (getModConfig().Enabled.GetValue() && getModConfig().YeetCrouchWalls.GetValue() && obstacleData->lineLayer != 0)
     {
