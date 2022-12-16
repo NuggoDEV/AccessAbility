7c816d034ee4226bf92bd1f7fe8ef924bad9c86b
NuggoDEV <NuggoDEV@gmail.com>
2022-12-16 19:57:03 +1030

    Allow for cripple mode

--- include/ModConfig.hpp
+++ include/ModConfig.hpp
@@ -8,4 +8,6 @@ DECLARE_CONFIG(ModConfig,
     CONFIG_VALUE(RightSaberToggle, bool, "Right Saber Enabled", false);
     CONFIG_VALUE(YeetCrouchWalls, bool, "Disable Crouch Walls", false);
     CONFIG_VALUE(YeetBombs, bool, "Yeets Bombs", false);
+    CONFIG_VALUE(CrippleMode, bool, "Cripple Mode", false);
+    CONFIG_VALUE(HeightPreCrippleMode, float, "Height Before Cripple Mode Invaded", 0.0f);
 )
