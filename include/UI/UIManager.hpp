9cb9dedcaabdb158e756a8850f62b2dc713a8b43
NuggoDEV <NuggoDEV@gmail.com>
2022-12-31 00:51:43 +1030

    Das enough for one night

--- include/UI/UIManager.hpp
+++ include/UI/UIManager.hpp
@@ -9,8 +9,16 @@
 #include "UnityEngine/Color.hpp"
 using namespace UnityEngine;
 
+#define GET_FIND_METHOD(mPtr) il2cpp_utils::il2cpp_type_check::MetadataGetter<mPtr>::get()
+
 DECLARE_CLASS_CODEGEN(AccessAbility::UI, UIManager, Il2CppObject,
     DECLARE_INSTANCE_FIELD(HMUI::ViewController*, settingsViewController);
+
+    DECLARE_INSTANCE_METHOD(void, ScoringTextEnabled, bool isEnabled);
+    DECLARE_INSTANCE_METHOD(void, CheckScoreSub);
+
+    DECLARE_INSTANCE_FIELD(UnityEngine::GameObject*, ScoreTextE);
+    DECLARE_INSTANCE_FIELD(UnityEngine::GameObject*, ScoreTextD);
 
     DECLARE_BSML_PROPERTY(bool, ModToggle);
     DECLARE_BSML_PROPERTY(bool, LeftSaberToggle);
--- src/UI/UIManager.cpp
+++ src/UI/UIManager.cpp
@@ -1,6 +1,8 @@
+#include "main.hpp"
 #include "UI/UIManager.hpp"
 
 #include "ModConfig.hpp"
+#include "bs-utils/shared/utils.hpp"
 
 #include "UnityEngine/Color.hpp"
 using namespace UnityEngine;
@@ -22,20 +24,83 @@ namespace AccessAbility::UI
 
     }
 
+
+
+    void UIManager::ScoringTextEnabled(bool isEnabled)
+    {
+        
+        
+    }
+
+    void UIManager::CheckScoreSub()
+    {
+        if (!bs_utils::Submission::getEnabled())
+        {
+            ScoreTextE->SetActive(false);
+            ScoreTextD->SetActive(true);
+        }
+        else
+        {
+            ScoreTextE->SetActive(true);
+            ScoreTextD->SetActive(false);
+        }
+        //if (getModConfig().ModToggle.GetValue() and getModConfig().LeftSaberToggle.GetValue() or getModConfig().RightSaberToggle.GetValue() or getModConfig().YeetCrouchWalls.GetValue() or getModConfig().YeetBombs.GetValue())
+        //{
+        //    bs_utils::Submission::disable(ModInf());
+        //    ScoreTextE->SetActive(false);
+        //    ScoreTextD->SetActive(true);
+        //}
+        //else
+        //{
+        //    bs_utils::Submission::enable(ModInf());
+        //    ScoreTextE->SetActive(true);
+        //    ScoreTextD->SetActive(false);
+        //}
+    }
+
+
     bool UIManager::get_ModToggle() { return getModConfig().ModToggle.GetValue(); }
     void UIManager::set_ModToggle(bool value) { getModConfig().ModToggle.SetValue(value); }
 
     bool UIManager::get_LeftSaberToggle() { return getModConfig().LeftSaberToggle.GetValue(); }
-    void UIManager::set_LeftSaberToggle(bool value) { getModConfig().LeftSaberToggle.SetValue(value); }
+    void UIManager::set_LeftSaberToggle(bool value) 
+    { 
+        getModConfig().LeftSaberToggle.SetValue(value); 
+        if (getModConfig().ModToggle.GetValue() && value)
+            bs_utils::Submission::disable(ModInf());
+        else
+            bs_utils::Submission::enable(ModInf());
+    }
 
     bool UIManager::get_RightSaberToggle() { return getModConfig().RightSaberToggle.GetValue(); }
-    void UIManager::set_RightSaberToggle(bool value) { getModConfig().RightSaberToggle.SetValue(value); }
+    void UIManager::set_RightSaberToggle(bool value) 
+    { 
+        getModConfig().RightSaberToggle.SetValue(value);
+        if (getModConfig().ModToggle.GetValue() && value)
+            bs_utils::Submission::disable(ModInf());
+        else
+            bs_utils::Submission::enable(ModInf());
+    }
 
     bool UIManager::get_YeetCrouchWalls() { return getModConfig().YeetCrouchWalls.GetValue(); }
-    void UIManager::set_YeetCrouchWalls(bool value) { getModConfig().YeetCrouchWalls.SetValue(value); }
+    void UIManager::set_YeetCrouchWalls(bool value) 
+    { 
+        getModConfig().YeetCrouchWalls.SetValue(value);
+        if (getModConfig().ModToggle.GetValue() && value)
+            bs_utils::Submission::disable(ModInf());
+        else
+            bs_utils::Submission::enable(ModInf());
+    }
 
     bool UIManager::get_YeetBombs() { return getModConfig().YeetBombs.GetValue(); }
-    void UIManager::set_YeetBombs(bool value) { getModConfig().YeetBombs.SetValue(value); }
+    void UIManager::set_YeetBombs(bool value) 
+    { 
+        getModConfig().YeetBombs.SetValue(value);
+        if (getModConfig().ModToggle.GetValue() && value)
+            bs_utils::Submission::disable(ModInf());
+        else
+            bs_utils::Submission::enable(ModInf());
+    }
 
     bool UIManager::get_SeatedMode() { return getModConfig().SeatedMode.GetValue(); }
     void UIManager::set_SeatedMode(bool value) { getModConfig().SeatedMode.SetValue(value); }
